7dddb726b2756ece53f5222b1808f10fe043679b
Zulkifl Ajmal <zulkiflajmal@gmail.com>
2024-04-26 09:37:01 +0530

    placed the while loop file and examle in the correct folder

---
+++ lecture_3/while_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 1;
+    while (count <= 5)
+    {
+        cout << "Hello world!\n";
+        count++;
+    }
+    return 0;
+}
