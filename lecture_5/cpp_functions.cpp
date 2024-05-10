3348263eb26dc4bce334131503eee80a58f50fa5
Zulkifl Ajmal <zulkiflajmal@gmail.com>
2024-05-10 09:54:52 +0530

    created cpp_function.cpp

---
+++ lecture_5/cpp_functions.cpp
@@ -0,0 +1,28 @@
+/*
+Using functions we can devide our program into sub parts
+
+structure
+---------
+return_type funtion_name(parameter list)
+{
+    body of the code
+
+
+    return return_value;
+}
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void display()
+{
+}
+
+int main()
+{
+
+    return 0;
+}
