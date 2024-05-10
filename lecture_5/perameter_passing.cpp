11d60011bc401fd4ebdabf802b10130a3e1ef04b
Zulkifl Ajmal <zulkiflajmal@gmail.com>
2024-05-10 13:48:58 +0530

    addded perameter_passing.cpp

---
+++ lecture_5/perameter_passing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void calculate(int a, int b, int c)
+{
+    int tot = a + b + c;
+    cout << tot << endl;
+}
+int main()
+{
+    calculate(10, 11, 4);
+    return 0;
+}
